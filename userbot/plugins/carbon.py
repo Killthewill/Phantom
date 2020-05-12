735c40b4d9e4fc0053bc9196ccb6e6deb87fe4d1
Levin Rock <63445750+levingod@users.noreply.github.com>
2020-05-12 13:15:11 +0530

    Update carbon.py

--- userbot/plugins/carbon.py
+++ userbot/plugins/carbon.py
@@ -140,7 +140,7 @@ async def carbon_api(e):
 
    sleep(2.5)
 
-   await e.edit("`Done Dana Done...\n100%`")
+   await e.edit("`Completed...\n100%`")
 
    file = './carbon.png'
 
@@ -152,7 +152,7 @@ async def carbon_api(e):
 
          file,
 
-         caption="<< Here's your carbon, \n Carbonised by [IndianBot](https://www.github.com/indianbhaiya/IndianBot)>> ",
+         caption="<< Here's your carbon, \n Carbonised by [Phantom](https://www.github.com/levingod/Phantom)>> ",
 
          force_document=True,
 
@@ -160,7 +160,7 @@ async def carbon_api(e):
 
          )
 
-   os.remove('./IndianBot.png')
+   os.remove('./Phanthomt.png')
 
    driver.quit()
 
